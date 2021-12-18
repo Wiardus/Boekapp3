9d4af4396f9a3b2c3f78e242073c8ca1d0ce5e91
Wiardus <ynte.braaksma@student.nhlstenden.com>
2021-12-18 11:16:49 +0100

    Initial commit

---
+++ db/migrate/20211217132405_create_books.rb
@@ -0,0 +1,9 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.references :author, null: false, foreign_key: true
+      t.string :title
+      t.timestamps
+    end
+  end
+end
